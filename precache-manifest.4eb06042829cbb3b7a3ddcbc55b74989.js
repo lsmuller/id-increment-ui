815b7aea8fec5c1496d2bb73b5f045ea6f915a18
Leticia Muller <leticia.muller@sap.com>
2019-02-10 20:03:19 -0200

    Updates

---
+++ precache-manifest.4eb06042829cbb3b7a3ddcbc55b74989.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69bc974dc70e2539ebd2",
+    "url": "/id-increment-ui/static/css/main.a9fc67b1.chunk.css"
+  },
+  {
+    "revision": "69bc974dc70e2539ebd2",
+    "url": "/id-increment-ui/static/js/main.69bc974d.chunk.js"
+  },
+  {
+    "revision": "fb9a2ee13128178921b2",
+    "url": "/id-increment-ui/static/js/1.fb9a2ee1.chunk.js"
+  },
+  {
+    "revision": "146cfc7e16407baec3e0",
+    "url": "/id-increment-ui/static/js/runtime~main.146cfc7e.js"
+  },
+  {
+    "revision": "cf3443f5c289b2f4aab76ee66d7b87fb",
+    "url": "/id-increment-ui/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/id-increment-ui/precache-manifest.4eb06042829cbb3b7a3ddcbc55b74989.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/id-increment-ui/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
